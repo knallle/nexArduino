14e6a22bbfcdaa5f3298972517df2c409107611b
Kalle Svensson <knallle.morrum@gmail.com>
2016-10-28 09:21:14 +0200

    Added support for setting an alarm. Changed the way to communicate with Arduino. Now sends one byte at the time. End message with '%'

--- www/app.py
+++ www/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import serial
 import time
+import json
 
 app = Flask(__name__)
 
@@ -18,54 +19,50 @@ def write_dummy_alarm():
 	f.write(command)
 	f.close
 	
+def set_alarm(device, state, dim_level, min_on, hour_on, min_off, hour_off):
+	f = open('/var/www/alarm_json','r')
+	json_str = f.read()
+	f.close()
+	units = json.loads(json_str)
+	units[device]['min_on'] = min_on	
+	units[device]['hour_on'] = hour_on	
+	units[device]['min_off'] = min_off	
+	units[device]['hour_off'] = hour_off	
+	units[device]['state'] = state
+	units[device]['dim_level'] = dim_level
+	f = open('/var/www/alarm_json','w')
+	f.write( json.dumps(units) )
+	f.close()
 
 @app.route('/')
 def index():
-	return render_template('lamps.html')
+	return render_template('lamps_empty.html')
 
 @app.route('/<device>/<state>/<dim_level>')
 def update_lamp(device, state, dim_level):
 	if device.isdigit() and state.isdigit() and dim_level.isdigit():
-		delimiter = ','
-		msg = str(device) + delimiter + str(state) + delimiter + str(dim_level)
 		
-		ser.write(msg)
-		time.sleep(0.3)
+		delimiter = "/"
+		eol = "%"
 
-	return render_template('lamps.html')
+		msg = str(device) + delimiter + str(state) + delimiter + str(dim_level) + eol
+
+		for m in msg:
+			ser.write(m)
+			time.sleep(0.005)
+		
+	return render_template('lamps_empty.html')
 
 @app.route('/<device>/<state>/<dim_level>/<schedule_h_on>/<schedule_m_on>/<schedule_h_off>/<schedule_m_off>')
 def schedule_lamp(device, state, dim_level, schedule_h_on, schedule_m_on, schedule_h_off, schedule_m_off):
-	s_h_on = int(schedule_h_on)
-	s_m_on = int(schedule_h_on)
-	s_h_off = int(schedule_h_off)
-	s_m_off = int(schedule_h_off)
-	
-	if s_h_on>=0 and s_m_on>=0 and s_h_off>=0 and s_m_off>=0:
-		command = "/".join( (device, state, dim_level) ) + "\n"
-		f = open('.alarm', 'w')
-		f.write(command)
-		f.close
+	hour_on = int(schedule_h_on)
+	min_on = int(schedule_m_on)
+	hour_off = int(schedule_h_off)
+	min_off = int(schedule_m_off)
 
-		if s_h_on <= 24 and s_m_on <= 60:
-			# Set timer to start lamp
-			command = "/".join( (schedule_h_on, schedule_m_on) ) + "\n"
-			f = open('.alarm', 'a')
-			f.write(command)
-			f.close
-		else:
-			write_dummy_alarm()	
+	set_alarm(device, state, dim_level, min_on, hour_on, min_off, hour_off)
 
-		if s_h_off <= 24 and s_m_off <= 60:
-			# Set timer to shut down lamp
-			command = "/".join( (schedule_h_off, schedule_m_off) ) + "\n"
-			f = open('.alarm', 'a')
-			f.write(command)
-			f.close
-		else:
-			write_dummy_alarm()	
-
-	return render_template('lamps.html')	
+	return render_template('lamps_empty.html')	
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0', threaded=True)
