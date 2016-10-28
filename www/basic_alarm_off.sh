f5e45bdfd32e57e8003fb85899180f1f7a613de7
Kalle Svensson <knallle.morrum@gmail.com>
2016-10-28 09:21:52 +0200

    Wrote basic scripts to turn on/off certain lamps in my home

---
+++ www/basic_alarm_off.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#curl -s 192.168.1.249:5000/1/0/99 > /dev/null
+#sleep .5
+curl -s 192.168.1.249:5000/6/0/99 > /dev/null
+sleep .5
+curl -s 192.168.1.249:5000/3/0/99 > /dev/null
+sleep .5
+curl -s 192.168.1.249:5000/5/0/99 > /dev/null
---
+++ www/basic_alarm_on.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#curl -s 192.168.1.249:5000/1/1/10 > /dev/null
+#sleep .5
+curl -s 192.168.1.249:5000/6/1/99 > /dev/null
+sleep .5
+curl -s 192.168.1.249:5000/3/1/99 > /dev/null
+sleep .5
+curl -s 192.168.1.249:5000/5/1/99 > /dev/null
